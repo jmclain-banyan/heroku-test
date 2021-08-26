b192d7a5767241c0ce55361e35bde632b764c548
Josh McLain <joshmclain45@gmail.com>
2021-08-25 19:35:13 -0700

    setup basic express server

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello universe!</h1>");
+});
+
+app.listen(port, () => console.log(`app running on port ${port}`));
